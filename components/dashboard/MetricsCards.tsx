dd3d3b19a1e0e0f72b7ac7ea730489eec07368e3
Chirag Bangera <cbepic2@gmail.com>
2024-11-29 13:19:13 -0500

    Add delete back to metrics card

--- components/dashboard/MetricsCards.tsx
+++ components/dashboard/MetricsCards.tsx
@@ -362,6 +362,14 @@ export function MetricsCards({ transactions, categories }: MetricsCardsProps) {
                       >
                         <Pencil className="h-4 w-4" />
                       </Button>
+                      <Button
+                        variant="ghost"
+                        size="icon"
+                        className="h-8 w-8 opacity-0 group-hover:opacity-100 transition-opacity"
+                        onClick={() => handleDeleteCard(metric.id)}
+                      >
+                        <Trash2 className="h-4 w-4" />
+                      </Button>
                       {metric.icon}
                     </div>
                   </CardHeader>
